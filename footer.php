9fcc55095e1088bb41754a30484167b6154e8b4a
Weic Ma <i@weic.me>
2018-06-14 22:16:25 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="blog-footer">
     <p class="blog-footer-left">
         Copyright © <?php echo(date("Y")) ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a> <?php if (get_option( 'zh_cn_l10n_icp_num' )){ ?>| <a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank"><?php echo get_option( 'zh_cn_l10n_icp_num' );?></a> <?php } ?></p>
-    <p class="blog-footer-right">Powered by <a href="https://wordpress.org" target="_blank">WordPress</a>,Theme <a href="https://weic.me/themes-life/" target="_blank">Life</a></p>
+    <p class="blog-footer-right">Powered by <a href="https://wordpress.org" target="_blank">WordPress</a>,Theme <a href="https://note.isweic.com/themes-life/" target="_blank">Life</a></p>
 </footer>
 
 </div><!--博客主栏结束-->
